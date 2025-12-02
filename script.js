215118a2487c8fe04b4cfcb4700bc40228ef455a
Abdul Wahab <write2wahab786@gmail.com>
2025-12-02 17:14:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine','ejs');
+
+app.get('/', function(req,res){
+    res.render("index");
+});
+
+app.get('/profile/:username', function(req,res){
+    res.send(req.params.username);
+});
+
+
+
+app.listen(3000);
